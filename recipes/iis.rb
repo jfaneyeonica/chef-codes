f1c6676433d16a0d611ff62c0b807153f74dc7bb
John Faneye <49312666+jfaneyeonica@users.noreply.github.com>
2020-09-20 17:17:36 -0400

    add iis recipe

--- recipes/iis.rb
+++ recipes/iis.rb
@@ -7,7 +7,18 @@ powershell_script 'Install IIS' do
     action [:enable, :start]
   end
   
-  template 'c:inetpubwwwrootDefault.htm' do
-    source 'index.html.erb'
-    rights :read, 'Everyone'
-  end
+#   template 'c:inetpubwwwrootDefault.htm' do
+#     source 'index.html.erb'
+#     rights :read, 'Everyone'
+#   end
+
+file '/var/www/html/index.html' do
+    content '<html>
+    <head>
+    <title> This is simple webpage </title>
+    </head>
+    <body>
+    <h1> This is a special website </h1>
+    </body>
+</html>'
+end
